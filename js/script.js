fb32db9ab9e7f29d5159fc985db4ded85e9fb584
Titho93 <titho10.93@gmail.com>
2023-09-13 14:54:59 +0200

    [add]image with js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const imageCont  = document.querySelector('.imageCont');
+const btnNext = document.querySelector('.down');
+const btnPrev = document.querySelector('.up');
+
+const images  = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+];
+
+for (let i = 0; i < images.length; i++){
+  const image = images[i];
+  imageCont.innerHTML += `<img src="${image}" class= item hide >`;
+}
