db5c5914f67f3137bc71b698cee63d8b169ce1d1
Titho93 <titho10.93@gmail.com>
2023-09-22 16:01:35 +0200

    [add]class active

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const imageCont  = document.querySelector('.imageCont');
 const btnNext = document.querySelector('.down');
 const btnPrev = document.querySelector('.up');
-const thumb = document.querySelector('.thumb');
+let thumb = document.querySelector('.thumb');
 
 btnPrev.classList.add('hide');
 
@@ -18,19 +18,26 @@ const images  = [
 for (let i = 0; i < images.length; i++){
   const image = images[i];
   imageCont.innerHTML += `<img src="${image}" class="item hide">`;
+  thumb.innerHTML += `<img src="${image}" class="object">`;
 }
 
 
 const imageCollect = document.getElementsByClassName('item');
+const thumbCollect = document.getElementsByClassName('object');
 
-imageCollect[counterImg].classList.remove('hide'); 
+imageCollect[counterImg].classList.remove('hide');
+thumbCollect[counterImg].classList.add('active');
+
 
 btnNext.addEventListener('click', function(){
   imageCollect[counterImg].classList.add('hide');
+  thumbCollect[counterImg].classList.remove('active');
   counterImg++;
   imageCollect[counterImg].classList.remove('hide');
+  thumbCollect[counterImg].classList.add('active');
+
   btnPrev.classList.remove('hide');
-  
+
   if(counterImg === imageCollect.length - 1){
     btnNext.classList.add('hide');
   }
@@ -39,8 +46,10 @@ btnNext.addEventListener('click', function(){
 
 btnPrev.addEventListener('click', function(){
   imageCollect[counterImg].classList.add('hide');
+  thumbCollect[counterImg].classList.remove('active');
   counterImg--;
   imageCollect[counterImg].classList.remove('hide');
+  thumbCollect[counterImg].classList.add('active');
 
   btnNext.classList.remove('hide')
 
